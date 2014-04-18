56540d4d85fdbf52754ff9bddc2a2c3ff3132525
Jasper van Rijbroek <jasper199069@live.nl>
2014-04-18 16:01:40 +0200

    Added better route handling.

--- packages/component/administrator/components/com_kutafuta/databases/behaviors/searchable.php
+++ packages/component/administrator/components/com_kutafuta/databases/behaviors/searchable.php
@@ -11,7 +11,7 @@ class ComKutafutaDatabaseBehaviorSearchable extends KDatabaseBehaviorAbstract
 	}
 
 	public function _beforeTableDelete(KCommandContext $context) {
-		if($id = $this->_parseUrlData()) {
+		if($id = $this->_parseUrlData($context->data)) {
 			$this->getService('com://admin/kutafuta.model.terms')->route_id($id)->getList()->delete();
 		}
 	}
@@ -22,9 +22,11 @@ class ComKutafutaDatabaseBehaviorSearchable extends KDatabaseBehaviorAbstract
 		// Make sure everything is removed
 		$this->_beforeTableDelete($context);
 
+        $route_id = $this->_parseUrlData($context->data);
+
 		// Saving the title.
 		$this->getService('com://admin/kutafuta.database.row.term')->setData(array(
-			'route_id' => $this->_parseUrlData(),
+			'route_id' => $route_id,
 			'value' => $context->data->title,
 			'lang' => substr(JFactory::getLanguage()->getTag(), 0, 2)
 		))->save();
@@ -34,7 +36,7 @@ class ComKutafutaDatabaseBehaviorSearchable extends KDatabaseBehaviorAbstract
 			foreach($context->data->getElements() as $key => $value) {
 				if($filter->validate(strip_tags($value->value)) && !empty($value->value)) {
 					$this->getService('com://admin/kutafuta.database.row.term')->setData(array(
-						'route_id' => $this->_parseUrlData(),
+						'route_id' => $route_id,
 						'value' => strip_tags($value->value),
 						'lang' => substr(JFactory::getLanguage()->getTag(), 0, 2)
 					))->save();
@@ -43,9 +45,9 @@ class ComKutafutaDatabaseBehaviorSearchable extends KDatabaseBehaviorAbstract
 		}
 	}
 
-	private function _parseUrlData() {
-		$uri = JFactory::getURI();
+	private function _parseUrlData($data) {
+		$identifier = $data->getIdentifier();
 
-        return $this->getService('com://admin/routes.model.routes')->query($uri->getQuery())->lang(substr(JFactory::getLanguage()->getTag(), 0, 2))->getItem()->id;
+        return $this->getService('com://admin/routes.model.routes')->query('option=com_' . $identifier->package . '&view=' . $identifier->name . '&id=' . $data->id)->lang(substr(JFactory::getLanguage()->getTag(), 0, 2))->getItem()->id;
 	}
 }
