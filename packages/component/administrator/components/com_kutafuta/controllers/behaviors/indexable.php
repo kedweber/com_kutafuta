9887362e905378fb5c1f5601d97fe55ab810bcee
Jasper van Rijbroek <jasper199069@live.nl>
2014-04-18 14:36:41 +0200

    Added indexable controller behavior.

---
+++ packages/component/administrator/components/com_kutafuta/controllers/behaviors/indexable.php
@@ -0,0 +1,23 @@
+<?php
+
+class ComKutafutaControllerBehaviorIndexable extends KControllerBehaviorAbstract
+{
+    public function _actionIndex(KCommandContext $context)
+    {
+        // We will get all the items of the database and resave them to index everything.
+        set_time_limit(1440);
+
+        $modelIdentifier = clone $context->caller->getIdentifier();
+        $modelIdentifier->path = array('model');
+
+        $model = $this->getService($modelIdentifier);
+        $items = $model->limit(0)->getList();
+
+        foreach($items as $item)
+        {
+            $item->save();
+        }
+
+        JFactory::getApplication()->redirect(KRequest::referrer(), JText::_(count($items) . ' items are reindexed'));
+    }
+}
