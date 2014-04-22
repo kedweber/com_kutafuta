b414fef147473a661404c10dd77c1fe1a0ce7c6c
Jasper van Rijbroek <jasper199069@live.nl>
2014-04-22 14:41:31 +0200

    Fixed urls and fixed action add not allowed.

--- packages/component/components/com_kutafuta/views/terms/tmpl/default.php
+++ packages/component/components/com_kutafuta/views/terms/tmpl/default.php
@@ -1,21 +1,19 @@
 <? defined('KOOWA') or die('Restricted Access'); ?>
 
 <div class="com_search">
-    <form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_kutafuta&view=terms');?>" method="post">
+    <form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_kutafuta&view=terms');?>" method="get">
         <div class="input-group">
             <input type="text" name="search" placeholder="<?php echo JText::_('Keyword'); ?>" id="search-searchword" value="<?= @escape($state->search); ?>" class="form-control" />
             <span name="Search" onclick="getElementById('searchForm').submit()" class="btn hasTooltip input-group-addon" title="<?php echo JHtml::tooltipText('COM_SEARCH_SEARCH');?>">
                 <span class="icon-search"></span>
             </span>
         </div>
-
-        <input type="hidden" name="task" value="search" />
     </form>
 
     <ul class="results" id="container">
         <? foreach($terms as $result) : ?>
             <li>
-                <a href="<?php echo $result->href; ?>"><?php echo $result->getRelatedData()->title; ?></a>
+                <a href="<?php echo JRoute::_('index.php?'.$result->query); ?>"><?php echo $result->getRelatedData()->title; ?></a>
             </li>
         <? endforeach; ?>
     </ul>
