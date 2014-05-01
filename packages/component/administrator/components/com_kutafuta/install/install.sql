a217645e3c17a9b415aca1dd9a930821e7e73602
Jasper van Rijbroek <jasper199069@live.nl>
2014-05-01 14:49:00 +0200

    Updated install.sql for kutafuta.

--- packages/component/administrator/components/com_kutafuta/install/install.sql
+++ packages/component/administrator/components/com_kutafuta/install/install.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS `#__kutafuta_terms`;
 CREATE TABLE `#__kutafuta_terms` (
   `kutafuta_term_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `route_id` bigint(20) NOT NULL,
   `value` text NOT NULL,
   `lang` varchar(255) NOT NULL,
+  `table` varchar(255) NOT NULL,
+  `row` bigint NOT NULL
   PRIMARY KEY (`kutafuta_term_id`),
   FULLTEXT KEY `search` (`value`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
